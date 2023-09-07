2f701b865b717f21f6eb694c718af7444bcdd956
MrSohan10 <iamsohan11@gmail.com>
2023-09-07 23:34:53 +0600

    some practiced appBar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,13 +3,28 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  mySnackbar(message, context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('My App'),
+        title: Text('App Bar'),
+        toolbarHeight: 60,
+        // titleSpacing: 10,
+        centerTitle: true,
+        elevation: 10,
+        shadowColor: Colors.blue,
+        backgroundColor: Colors.green,
+        leading: Icon(Icons.person),
+        actions: [
+          IconButton(onPressed: (){mySnackbar("i am message", context);}, icon: Icon(Icons.message)),
+          IconButton(onPressed: (){mySnackbar("i am menu", context);}, icon: Icon(Icons.menu)),
+        ],
       ),
-      body: Text('Home'),
     );
   }
 }
