1a9fc5961cbc2b30b29ab3b943566390bdb1222a
MrSohan10 <iamsohan11@gmail.com>
2023-09-07 23:54:51 +0600

    learned floating action button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,27 +3,26 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  mySnackbar(message, context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
+  mySnackbar(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('App Bar'),
-        toolbarHeight: 60,
-        // titleSpacing: 10,
-        centerTitle: true,
-        elevation: 10,
-        shadowColor: Colors.blue,
-        backgroundColor: Colors.green,
-        leading: Icon(Icons.person),
-        actions: [
-          IconButton(onPressed: (){mySnackbar("i am message", context);}, icon: Icon(Icons.message)),
-          IconButton(onPressed: (){mySnackbar("i am menu", context);}, icon: Icon(Icons.menu)),
-        ],
+        title: Text("Floating Action Button"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          mySnackbar("i'm floating action button", context);
+        },
+        child: Icon(Icons.add),
+        splashColor: Colors.redAccent,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        elevation: 20,
       ),
     );
   }
