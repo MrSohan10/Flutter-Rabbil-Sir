7bbc45b28044e3ee7e2cda4ae571ba27fac48e22
MrSohan10 <iamsohan11@gmail.com>
2023-09-10 23:51:37 +0600

    some practice navigation drawer

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,29 +12,54 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Bottom tab var"),
+        title: Text("Navigation drawer"),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        iconSize: 30,
-        backgroundColor: Colors.green,
-        selectedItemColor: Colors.amber,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
-          BottomNavigationBarItem(icon: Icon(Icons.call), label: 'Call'),
-        ],
-        onTap: (int index) {
-          if (index == 0) {
-            mySnackbar("Sohan", context);
-          }
-          if (index == 1) {
-            mySnackbar("rabby", context);
-          }
-          if (index == 2) {
-            mySnackbar("Sabbir", context);
-          }
-        },
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.all(3),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  accountName: Text('Mr. Sohan'),
+                  accountEmail: Text('iamsohan11@gmail.com'),
+                  currentAccountPicture: Image.network(
+                      "https://cdn.iconscout.com/icon/free/png-256/free-flutter-3628777-3030139.png?f=webp"),
+
+                onDetailsPressed: (){
+                    mySnackbar("i am drawer details", context);
+                },
+                )),
+            ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {
+                  mySnackbar("i'm home", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.message),
+                title: Text("message"),
+                onTap: () {
+                  mySnackbar("i'm message", context);
+                }),
+            ListTile(
+                leading: Icon(Icons.call),
+                title: Text("call"),
+                onTap: () {
+                  mySnackbar("i'm call", context);
+                }),
+            ListTile(
+              leading: Icon(Icons.mail),
+              title: Text("mail"),
+              onTap: () {
+                mySnackbar("i'm mail", context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
