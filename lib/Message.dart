98b63a89baf5a3bf888cffa9ecf9ae5079cff6c6
MrSohan10 <iamsohan11@gmail.com>
2023-09-17 08:02:35 +0600

    practice tab bar view

---
+++ lib/Message.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.green,
+          child: Center(
+            child: Text('i am Message'),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,70 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rabbil_sir/Breaking.dart';
+import 'package:flutter_rabbil_sir/Group.dart';
+import 'package:flutter_rabbil_sir/Home.dart';
+import 'package:flutter_rabbil_sir/Message.dart';
+import 'package:flutter_rabbil_sir/Phone.dart';
+import 'package:flutter_rabbil_sir/Profile.dart';
+import 'package:flutter_rabbil_sir/Store.dart';
+import 'package:flutter_rabbil_sir/Video.dart';
 
 class HomePage extends StatelessWidget {
-  var MyItems = [
-    {
-      'img':
-          'https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png',
-      'title': 'I am sohan'
-    },
-    {
-      'img':
-      'https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png',
-      'title': 'I am rabby'
-    },
-    {
-      'img':
-      'https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png',
-      'title': 'I am saad'
-    },
-    {
-      'img':
-      'https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png',
-      'title': 'I am fahim'
-    },
-    {
-      'img':
-      'https://www.mindinventory.com/blog/wp-content/uploads/2022/10/flutter-3.png',
-      'title': 'I am Rafi'
-    },
-  ];
-
-   MySnackvar (message, context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
-}
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dynamic List view'),
-        ),
-        body: Scrollbar(
-          thickness: 15,
-          radius: Radius.circular(10),
-          child: ListView.builder(
-
-            itemCount: MyItems.length,
-            itemBuilder: (context, index){
-              return GestureDetector(
-
-                onTap: (){MySnackvar(MyItems[index]['title'], context);},
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  height: 300,
-                  width: double.infinity,
-                  child: Image.network(MyItems[index]['img']!,fit: BoxFit.fill,),
-
-                ),
-
-              );
-            },
-          ),
+    return DefaultTabController(length: 8, child: Scaffold(
+      appBar: AppBar(
+        title: Text('TabBar'),
+        bottom: TabBar(
+          isScrollable: true,
+          tabs: [
+            Tab(icon: Icon(Icons.home),text: 'Home',),
+            Tab(icon: Icon(Icons.video_collection),text: 'Video',),
+            Tab(icon: Icon(Icons.newspaper),text: 'Breaking',),
+            Tab(icon: Icon(Icons.message),text: 'Message',),
+            Tab(icon: Icon(Icons.phone),text: 'Phone',),
+            Tab(icon: Icon(Icons.group),text: 'Group',),
+            Tab(icon: Icon(Icons.store),text: 'Store',),
+            Tab(icon: Icon(Icons.person),text: 'Profile',),
+          ],
         ),
       ),
+      body: TabBarView(
+        children: [
+          Home(),
+          Video(),
+          Breaking(),
+          Message(),
+          Phone(),
+          Group(),
+          Store(),
+          Profile()
 
-    );
+        ],
+      ),
+    ));
   }
 }
