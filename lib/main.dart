d58d1383f8e04ce3bbf7212ced2553c79f4c1736
MrSohan10 <iamsohan11@gmail.com>
2023-10-10 08:08:06 +0600

    practice flutter bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:responsive_grid/responsive_grid.dart';
+import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,143 +12,128 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Responsive Gride'),
-          ),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: ResponsiveGridRow(
-              children: [
-                ResponsiveGridCol(
-                  xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Container(
-                    height: 100,
-                    color: Colors.pink,
-                  ),
-                )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.black,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.green,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.yellow,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.blue,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.brown,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.purpleAccent,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.cyanAccent,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.black,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.pink,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.cyan,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.deepPurpleAccent,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.amberAccent,
-                      ),
-                    )),
-                ResponsiveGridCol(
-                    xl: 2,lg: 2, md: 3,sm: 6,xs: 12,
-                    child: Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 100,
-                        color: Colors.pink,
-                      ),
-                    )),
-              ],
-            ),
-          )),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Responsive Gride'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: BootstrapContainer(
+          fluid: true,
+          children: [
+            BootstrapRow( children: [
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding:EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding:EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding:EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.red,
+                    ),
+                  )),
+              BootstrapCol(
+                  sizes: 'col-xl-1 col-lg-2 col-md-3 col-sm-4 col-6',
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                    child: Container(
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  )),
+            ])
+          ],
+        ),
+      ),
+    ));
   }
 }
